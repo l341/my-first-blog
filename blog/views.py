5e8a0ac4e8d7bae25b7f7bfcf190b68c4bf67b5e
laura <lauracarreras96@gmail.com>
2019-11-19 17:25:35 +0100

    Added views to create/edit blog posts inside the site

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from .forms import PostForm
 
 # Create your views here.
 
@@ -17,3 +19,32 @@ def post_detail (request, pk):
     # Si no existe el archivo te lleva a la pagina de error
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # Aun no se qiere guardar, se quiere añadir un autor
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            # Normalmente se utiliza form.save pero es este caso este
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
